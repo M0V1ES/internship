876176e11c3ed9889b46f605fa7a908fc1425e3f
M0V1ES <valera.lomov2016@yandex.ru>
2024-04-07 18:14:55 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,41 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    Case1 case1 = new Case1();
-    }
-
-
-}
-
-class Case1{
-    public Case1(){
-        Map<String, String> row1 = new HashMap<>();
-        List<Map<String,String>> table = new ArrayList<Map<String,String>>();
-        row1.put("Имя", "Кирилл");
-        row1.put("Возраст", "26");
-        row1.put("Должность", "мидл");
-        row1.put("Зарплата", "15000 руб");
-        table.add(row1);
-
-        Map<String, String> row2 = new HashMap<>();
-        row2.put("Имя", "Виталий");
-        row2.put("Возраст", "28");
-        row2.put("Должность", "Сеньор");
-        row2.put("Зарплата", "2000 $");
-        table.add(row2);
-
-        Map<String, String> row3 = new HashMap<>();
-        row3.put("Имя", "Александр");
-        row3.put("Возраст", "31");
-        row3.put("Должность", "Джуниор");
-        row3.put("Зарплата", "50000 руб");
-        table.add(row3);
-
-        Map<String, String> row4 = new HashMap<>();
-        row4.put("Имя", "Дементий");
-        row4.put("Возраст", "35");
-        row4.put("Должность", "девопс");
-        row4.put("Зарплата", "1500 $");
-        table.add(row4);
-
-        System.out.println(SortbyAge(table));
-        System.out.println(SalaryinRub(table));
-        System.out.println(MeanAge(table));
-
-    }
-
-    public static List<Map<String,String>> SortbyAge( List<Map<String,String>>table){
-        List<Map<String,String>> sortedTable = new ArrayList<>();
-        for (int i = 0; i < table.size(); i++) {
-            if(Integer.parseInt(table.get(i).get("Возраст")) < 30){
-                sortedTable.add(table.get(i));
-            }
-        }
-        return sortedTable;
-    }
-
-    public static List<Map<String,String>> SalaryinRub( List<Map<String,String>> table ){
-        List<Map<String,String>> sortedTable = new ArrayList<>();
-        for (int i = 0; i < table.size(); i++) {
-            if (table.get(i).get("Зарплата").split(" ")[1].equals("руб")){
-                sortedTable.add(table.get(i));
-            }
-
-        }
-        return sortedTable;
-    }
-
-    public static int MeanAge(List<Map<String,String>> table){
-        int age = 0;
-        for (int i = 0; i < table.size(); i++) {
-            age += Integer.parseInt(table.get(i).get("Возраст"));
-        }
-        return (age / table.size());
+    Case2 case2 = new Case2(5);
     }
 }
+class Case2{
+    public Case2(int n){
+        int[][] array = new int[n][n];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int x = rand.nextInt(100);
+                array[i][j] = x;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+        System.out.printf("%5d", array[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println(FindNumber(array));
+    }
+
+
+    public static int FindNumber(int[][] array){
+        int min = array[3][0];
+        for (int i = 1; i < array.length; i++) {
+            if (i != 2) {
+                if (min > array[array.length - 1 - i][i]) {
+                    min = array[array.length - 1 - i][i];
+                }
+            }
+        }
+        return min;
+    }
+}
+
+
